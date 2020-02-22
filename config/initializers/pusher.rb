d272708c83d3e49e913d369af08f9c35ab873b65
Ricardo Maya <rickmaya@gmail.com>
2020-02-21 23:02:23 -0600

    first commit

---
+++ config/initializers/pusher.rb
@@ -0,0 +1,8 @@
+require 'pusher'
+
+Pusher.app_id = '952051'
+Pusher.key = 'bd42446b31cddb1ae458'
+Pusher.secret = '96b35f6ca338a35607da'
+Pusher.cluster = 'us2'
+Pusher.logger = Rails.logger
+Pusher.encrypted = true
